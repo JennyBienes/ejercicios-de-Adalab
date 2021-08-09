0d597c1f3e81cb5f435c7f5491d58de52919f4ac
Jenny <bienesjenny@gmail.com>
2021-08-09 20:29:33 +0200

    Finish exercise pair Kata

--- modulo-2/modulo-2-leccion-02-pair-kata/js/main.js
+++ modulo-2/modulo-2-leccion-02-pair-kata/js/main.js
@@ -1,17 +1,9 @@
 'use strict';
-document.querySelector(<span>Texto sin formato:</span>)
-const 
 
-// Objetivo:
-//   Debéis poner el texto que está en el span de la derecha del primer div
-// dentro del span del segundo div, transformando el texto en mayúsculas y
-// usando unicamente JavaScript (podéis poner clases en las etiquetas del HTML).
-// Pista: buscad en MDN los métodos de strings.
-//
-// Pasos:
-//
-// 1. Traer el <span> con el texto original a una constante.
-// 2. Traer el <span> donde se escribirá el texto modificado.
-// 3. Obtener el texto que contiene.
-// 4. Transformar el texto a mayúsculas.
-// 5. Poner el texto transformado en el <span> destino.
+const containerOrigin = document.querySelector('.js_origin');
+const containerTarget = document.querySelector('.js_target');
+
+const text = containerOrigin.innerHTML;
+const upperText = text.toUpperCase();
+
+containerTarget.innerHTML = upperText;
---
+++ modulo-2/modulo-2-leccion-4-ejercicio-5/main.js
@@ -0,0 +1,7 @@
+'use strict'
+
+function multi(a, b) {
+    return a * b;
+  }
+const multiResult = multi(40,10)
+console.log('cantidad', multiResult);
