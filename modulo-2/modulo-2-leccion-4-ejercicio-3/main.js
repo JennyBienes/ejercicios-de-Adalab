c518756a2449556fb017e46f6f901a9b070069ad
Jenny <bienesjenny@gmail.com>
2021-08-10 11:28:09 +0200

    Change exercise

--- modulo-2/modulo-2-leccion-4-ejercicio-3/main.js
+++ modulo-2/modulo-2-leccion-4-ejercicio-3/main.js
@@ -1,9 +1,12 @@
 'use strict'
 
-function price(a) {
-  return (a) ;
-}
-const resultPrice = (380);
-const priceIva = (380 * (21 / 100) );
-const priceTotal = resultPrice + priceIva;
-console.log(`El precio sin IVA ${resultPrice} , el IVA (21%) ${priceIva} y el total ${priceTotal}`);
+function precioconIva(precio) {
+ 
+
+const priceIva = (precio * (21 / 100) );
+const priceTotal = precio + priceIva;
+console.log(`El precio sin IVA ${precio} , el IVA (21%) ${priceIva} y el total ${priceTotal}`);
+return (priceTotal) ;}
+
+precioconIva(380)
+precioconIva(1000)
