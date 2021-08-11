cbab9c8a24d614365ed0e9bf18af1da80c37223c
Jenny <bienesjenny@gmail.com>
2021-08-11 18:43:01 +0200

    Add exercise 1 lesson 5 module 2

--- modulo-2/modulo-2-leccion-5-ejercicio-1/main.js
+++ modulo-2/modulo-2-leccion-5-ejercicio-1/main.js
@@ -1,27 +1,16 @@
 'use strict'
 
-let texttoshow = 'Hola Jenny'; 
-document.querySelector('h1').innerHTML =  texttoshow;
-let bottonselected=0
-
-//Elemento de HTML que se escucha Listener
-const button1 = document.querySelector('.ClickButton1');
-const button2 = document.querySelector('.ClickButton2');
-
-
-// Handler o manipulador
-function activebutton1 () {
-  console.log ('Boton 1 funcionando');
-  return document.querySelector('h1').innerHTML = "Mi primer click. Boton 1 activado";
-}
-
-function activebutton2 () {
-  console.log ('Boton 2 funcionando');
-  return document.querySelector('h1').innerHTML = "Mi primer click. Boton 2 activado";
-}
+const adalaber1 = {
+  name: 'Susana',
+  age: '34',
+  job: 'journalist'
+};
+console.log(adalaber1);
 
 
-
-//listener sobre el elemento, con tipo de evento y handler
-button1.addEventListener('click', activebutton1);
-button2.addEventListener('click', activebutton2);
+const adalaber2 = {
+  name: 'Rocío',
+  age: '25',
+  job: 'actress'
+};
+console.log(adalaber2);
