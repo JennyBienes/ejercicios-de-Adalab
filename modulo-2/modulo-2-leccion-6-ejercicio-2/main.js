5692eb6e362312ffde01633aa9789051de0292d4
Jenny <bienesjenny@gmail.com>
2021-08-12 11:20:41 +0200

    Add exercise module 2 lesson 6

--- modulo-2/modulo-2-leccion-6-ejercicio-2/main.js
+++ modulo-2/modulo-2-leccion-6-ejercicio-2/main.js
@@ -3,12 +3,23 @@
 const adalaber1 = {}
   adalaber1.name = 'Susana';
   adalaber1.age = '34';
-  adalaber1.job = 'journalist';
+  adalaber1.job = 'journalist'
+  adalaber1.run = function (){
+     console.log('Estoy corriendo');
+  }
+  adalaber1.run = ()=>  console.log('Estoy corriendo');
+  adalaber1.run();
+  
+  adalaber1.runAMarathon= function (distance){
+    console.log(`Èstoy corriendo un marathon de ${distance}`);
+  }
+  adalaber1.runAMarathon= distance => console.log(`Èstoy corriendo un marathon de ${distance}`);
+
+  adalaber1.runAMarathon(50);
+
+  adalaber1.runAMarathon(30);
 
 
-  const phrase = 'run'
-  adalaber1.run = `Yo estoy: ${phrase}`;
-  //muestra en la consola `Estoy corriendo`
-  console.log(adalaber1.run ('Estoy corriendo'));
+ 
 
   
