49ac566f1999954b319f3adbe0e0aa23cf3a13d3
Jenny <bienesjenny@gmail.com>
2021-09-18 10:35:52 +0200

    Add new exercise of pair

---
+++ modulo-3/promo-nerea-module-3-pair-14-hangman-game/src/components/App.js
@@ -0,0 +1,10 @@
+import avatar from '../images/logo.svg';
+import '../stylesheets/main.scss';
+
+function App() {
+  return <div className="App">
+<img src= {avatar} alt='logo'></img>;
+    </div>;
+}
+
+export default App;
