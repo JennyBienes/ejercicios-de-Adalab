a342a2a5658571acb93b4e76870f5d81f6607da7
Jenny <bienesjenny@gmail.com>
2021-09-27 23:04:09 +0200

    Add module 3 lesson 7 exercise 8

---
+++ modulo-3/ejercicio-3-7-1-8/src/components/Button.js
@@ -0,0 +1,13 @@
+const Button = (props) => {
+ 
+    return (
+      <>
+        <button className={props.ButtonClass} onClick={props.handleChange}>
+          {props.title}
+        </button>
+      </>
+    );
+  };
+  
+  export default Button;
+  
